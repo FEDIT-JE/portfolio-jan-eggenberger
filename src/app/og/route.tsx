2ba683d5a936bf09cd32f8ac3ccc23bfdaa015ec
Jan Eggenberger <j.eggenberger@federerag.ch>
2025-03-20 09:43:28 +0100

    remove route

--- src/app/og/route.tsx
+++ src/app/og/route.tsx
@@ -51,44 +51,8 @@ export async function GET(request: Request) {
             gap: "5rem",
           }}
         >
-          <img
-            src={"https://" + baseURL + person.avatar}
-            style={{
-              width: "12rem",
-              height: "12rem",
-              objectFit: "cover",
-              borderRadius: "100%",
-            }}
-          />
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              gap: "0.75rem",
-            }}
-          >
-            <span
-              style={{
-                fontSize: "4.5rem",
-                lineHeight: "4.5rem",
-                whiteSpace: "pre-wrap",
-                textWrap: "balance",
-              }}
-            >
-              {person.name}
-            </span>
-            <span
-              style={{
-                fontSize: "2.5rem",
-                lineHeight: "2.5rem",
-                whiteSpace: "pre-wrap",
-                textWrap: "balance",
-                opacity: "0.6",
-              }}
-            >
-              {person.role}
-            </span>
-          </div>
+          
+          
         </div>
       </div>
     </div>,
